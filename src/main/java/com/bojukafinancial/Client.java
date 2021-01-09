c1483ea933e97b38d730978d19986f28f6e446f9
Gabriel Fernandes Rosa Bojikian <gabriel.f.r.bojikian@gmail.com>
2021-01-09 11:37:42 -0300

    Added decorators, getters/setters to Client.java
    Added decorators to this class in order to use the new dependencies.
    This will make the Client object and database entity (and a bean),
    thus allowing the Springboot application context to handle it.
    
    Also added getters and setters to all attributes.

--- src/main/java/com/bojukafinancial/Client.java
+++ src/main/java/com/bojukafinancial/Client.java
@@ -1,16 +1,47 @@
 package com.bojukafinancial;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.math.BigDecimal;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
-	@Getter @Setter private String clientId; //Must be unique
-	@Getter @Setter private String name;
+	private @Id @GeneratedValue long clientId;
+	private String name;
+	private BigDecimal amountDue;
 	
-	Client(String clientId, String name){
+	Client() {}
+	
+	Client(long clientId, String name, BigDecimal amountDue){
 		this.clientId = clientId;
+		this.name = name;
+		this.amountDue = amountDue;
+	}
+	
+	public void setId(long clientId) {
+		this.clientId = clientId;
+	}
+
+	public long getId() {
+		return this.clientId;
+	}
+	
+	public void setName(String name) {
 		this.name = name;
 	}
 	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setAmountDue(BigDecimal amountDue) {
+		this.amountDue = amountDue;
+	}
+	
+	public BigDecimal getAmountDue() {
+		return this.amountDue;
+	}
 
 }
